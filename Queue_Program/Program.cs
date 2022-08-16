31f3b41476f94600d9be72287d0ff738259dc392
Saikiran Annangi <Saikiran_Annangi@epam.com>
2022-08-16 13:14:03 +0530

    Queue and Stack code

--- Queue_Program/Program.cs
+++ Queue_Program/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Queue_Program
 {
@@ -6,7 +7,46 @@ namespace Queue_Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create an empty Queue that holds strings
+            Queue<string> myQueue = new Queue<string>();
+
+            // Add items to the end of the queue
+            myQueue.Enqueue("one");
+            myQueue.Enqueue("two");
+            myQueue.Enqueue("three");
+            myQueue.Enqueue("four");
+            myQueue.Enqueue("five");
+
+            // use the count property to see how many items there are
+
+            Console.WriteLine("Count {0} ", myQueue.Count);
+            // Queues can be iterated without changing the structure
+            foreach (var item in myQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+            // peek at the front of the queue
+            string queueElement = myQueue.Peek();
+            Console.WriteLine(queueElement);
+            Console.WriteLine("Count {0} ", myQueue.Count);
+
+            // remove items from the front of the queue
+            queueElement = myQueue.Dequeue();
+            Console.WriteLine(queueElement);
+            Console.WriteLine("Count {0} ", myQueue.Count);
+            queueElement = myQueue.Dequeue();
+            Console.WriteLine(queueElement);
+            Console.WriteLine("Count {0} ", myQueue.Count);
+
+            // see if the queue contains an item
+            Console.WriteLine(myQueue.Contains("one"));
+            Console.WriteLine(myQueue.Contains("two"));
+            Console.WriteLine(myQueue.Contains("three"));
+
+            // Remove all the items
+            myQueue.Clear();
+            Console.WriteLine("Count {0} ", myQueue.Count);
         }
     }
 }
--- Stack_Program/Program.cs
+++ Stack_Program/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Stack_Program
 {
@@ -6,7 +8,37 @@ namespace Stack_Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TODO: Create an empty stack
+            Stack<string> myStack = new Stack<string>();
+
+            // TODO: The Push function pushes new elements onto the stack
+            myStack.Push("one");
+            myStack.Push("two");
+            myStack.Push("three");
+            myStack.Push("four");
+            myStack.Push("five");
+
+            // TODO: The Count property indicates how many elements are on the stack
+
+            Console.WriteLine("Count {0}", myStack.Count);
+            // TODO: Stacks can be enumerated in place without changing the content
+            foreach (var item in myStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            // TODO: The Peek function examines the current top element but does not remove it
+            string topElement = myStack.Peek();
+            Console.WriteLine(topElement);
+            Console.WriteLine("Count {0}", myStack.Count);
+            // TODO: The Pop function removes an element from the top
+            topElement = myStack.Pop();
+            Console.WriteLine(topElement);
+            Console.WriteLine("Count {0}", myStack.Count);
+
+            // TODO: Search for an element using Contains
+            Console.WriteLine(myStack.Contains("five"));
+            Console.WriteLine(myStack.Contains("four"));
         }
     }
 }
